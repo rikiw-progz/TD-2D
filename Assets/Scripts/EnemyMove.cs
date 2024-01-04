e40cc5a89fdf5e49340e7bb67e3f8d4f3cf70a6e
rikiw-progz <95848100+rikiw-progz@users.noreply.github.com>
2024-01-04 16:39:01 +0600

    04.01.24 enemy move bug

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,41 +5,66 @@ using DG.Tweening;
 
 public class EnemyMove : MonoBehaviour
 {
-    [SerializeField] private float[] pathTime;
-    private Coroutine p;
+    private int currentIndex = 0;
+    private Vector2 targetPosition;
+    public float speed = 5f;
 
-    private void OnEnable()
+    void Start()
     {
-        p = StartCoroutine(EnemyMoving());
+        MoveToNextPosition();
     }
 
-    private IEnumerator EnemyMoving()
+    void Update()
     {
-        this.transform.DOLocalMoveX(150f, pathTime[0]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[0]);
-        this.transform.DOLocalMoveY(100f, pathTime[1]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[1]);
-        this.transform.DOLocalMoveX(-50f, pathTime[2]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[2]);
-        this.transform.DOLocalMoveY(0f, pathTime[3]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[3]);
-        this.transform.DOLocalMoveX(50f, pathTime[4]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[4]);
-        this.transform.DOLocalMoveY(-100f, pathTime[5]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[5]);
-        this.transform.DOLocalMoveX(-150f, pathTime[6]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[6]);
-        this.transform.DOLocalMoveY(200f, pathTime[7]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[7]);
-        this.transform.DOLocalMoveX(1000f, pathTime[8]).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(pathTime[8]);
+        float step = speed * Time.deltaTime;
 
-        this.gameObject.SetActive(false);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+        if (Vector2.Distance(transform.localPosition, targetPosition) < 1f)
+        {
+            Debug.Log(0);
+            MoveToNextPosition();
+        }
+    }
+
+    void MoveToNextPosition()
+    {
+        switch (currentIndex % 9)
+        {
+            case 0:
+                targetPosition = new Vector2(150f, transform.position.y);
+                break;
+            case 1:
+                targetPosition = new Vector2(transform.position.x, 100f);
+                break;
+            case 2:
+                targetPosition = new Vector2(-50f, transform.position.y);
+                break;
+            case 3:
+                targetPosition = new Vector2(transform.position.x, 0f);
+                break;
+            case 4:
+                targetPosition = new Vector2(50f, transform.position.y);
+                break;
+            case 5:
+                targetPosition = new Vector2(transform.position.x, -100f);
+                break;
+            case 6:
+                targetPosition = new Vector2(-150f, transform.position.y);
+                break;
+            case 7:
+                targetPosition = new Vector2(transform.position.x, 200f);
+                break;
+            case 8:
+                targetPosition = new Vector2(1000f, transform.position.y);
+                break;
+
+        }
+        currentIndex++;
     }
 
     private void OnDisable()
     {
-        StopCoroutine(p);
         this.transform.position = new Vector2(-1000f, -200f);
     }
 }
